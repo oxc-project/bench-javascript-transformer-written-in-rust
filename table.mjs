f260fc687e4f4210c9e53796937b2c7f6cafce2f
Don Isaac <donald.isaac@gmail.com>
2024-09-23 18:48:55 -0400

    feat: plot benchmark results
    1. Add argument-baed options to table.mjs, letting users configure file format
      and/or output file path.
    2. Support CSV output
    3. Add a Jupyter notebook for plotting results

--- table.mjs
+++ table.mjs
@@ -1,3 +1,14 @@
+/**
+ * @file table.mjs
+ * @description Generate a table from criterion output.
+ * 
+ * Usage:
+ *   pnpm table [options]
+ * 
+ * # Options
+ *   -f,--format <format>  Output format. 'md' or 'csv'. Default: 'md'
+ *   -o,--output <path>    Output file path. Prints to stdout if not set.
+ */
 import fs from 'node:fs'
 import { markdownTable }  from 'markdown-table'
 
@@ -24,29 +35,113 @@ async function readData() {
 
   return data
 }
+/**
+ * @param {string[]} argv
+ */
+function parseArgs(argv) {
+  const opts = {
+    /**
+     * output format. Markdown or CSV.
+     * @type {'markdown' | 'csv'}
+     */
+    format: 'markdown',
+    /**
+     * Path to output file. `null` prints to stdout.
+     * @type {string | null}
+     */
+    output: null,
+  };
 
-async function main() {
+  for(let arg = argv.shift(); arg; arg = argv.shift()) {
+    switch(arg) {
+      case '-f':
+      case '--format': {
+
+        const format = argv.shift()?.trim()?.toLowerCase();
+        if (!format) throw new TypeError('--format flag requires an argument');
+        switch(format) {
+          case 'md':
+          case 'markdown':
+            opts.format = 'markdown';
+            break;
+          case 'csv':
+            opts.format = 'csv';
+            break;
+          default:
+            throw new TypeError(`Invalid format '${format}', expected 'md' or 'csv'`);
+        }
+        break;
+      }
+
+      case '-o':
+      case '--output': {
+        opts.output = argv.shift();
+        break;
+      }
+
+      // in case someone runs `pnpm table -- --format csv`
+      case '--':
+        continue
+    }
+  }
+
+  return opts;
+}
+
+async function main(argv) {
   const data = await readData();
   const groups = Object.keys(data);
   const columns = Object.keys(data[groups[0]]);
   const rows = Object.keys(data[groups[0]][columns[0]]);
+  const options = parseArgs(argv);
 
+  let out = '';
 
-  for (const group of groups) {
-    console.log(`### ${group}`);
-    console.log()
-    const table = [["", ...columns]];
-    for (const row of rows) {
-      const column_numbers = columns.map((column) => data[group][column][row].duration_ms);
-      const minimum = Math.min(...column_numbers);
-      const column_values = column_numbers.map((number) => {
-        return `\`${number.toFixed(1)} ms\` (${(number / minimum).toFixed(2)}x)`
-      });
-      table.push([row, ...column_values]);
+  switch(options.format) {
+    case 'markdown': {
+      for (const group of groups) {
+        out += `### ${group}\n`;
+        const table = [["", ...columns]];
+        for (const row of rows) {
+          const column_numbers = columns.map((column) => data[group][column][row].duration_ms);
+          const minimum = Math.min(...column_numbers);
+          const column_values = column_numbers.map((number) => {
+            return `\`${number.toFixed(1)} ms\` (${(number / minimum).toFixed(2)}x)`
+          });
+          table.push([row, ...column_values]);
+        }
+        out += markdownTable(table) + '\n';
+      }
+      break
     }
-    console.log(markdownTable(table));
-    console.log()
+
+    case 'csv': {
+      const header = ['group', 'bench_name', 'tool', 'measurement', 'duration_ms'];
+      out += header.join(',') + '\n';
+
+      for (const group of groups) {
+        // swc, oxc
+        for (const column of columns) {
+          const benches = data[group][column]
+          for (const bench in benches) {
+            const { duration_ms } = benches[bench];
+            out += `${group},${bench},${column},duration,${duration_ms}\n`;
+          }
+        }
+      }
+    }
+    break;
+
+    default:
+      throw new TypeError(`Unexpected output format '${options.format}'`);
+  }
+
+  if (!options.output) {
+    console.log(out);
+  } else {
+    await fs.promises.writeFile(options.output, out, 'utf8');
+    console.log(`Saved table to ${options.output}`);
   }
 }
 
-main()
+main(process.argv.slice(2)).catch(console.error);

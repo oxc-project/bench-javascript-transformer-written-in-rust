774b13ccee8fd94160df84ea28f4556542d192ac
Boshen <boshenc@gmail.com>
2024-11-26 00:27:00 +0800

    w

--- src/lib.rs
+++ src/lib.rs
@@ -35,10 +35,11 @@ pub mod swc {
     use std::{path::Path, sync::Arc};
 
     use swc::{try_with_handler, Compiler, PrintArgs, SwcComments};
-    use swc_common::{source_map::SourceMap, sync::Lrc, Mark, GLOBALS};
+    use swc_common::{errors::HANDLER, source_map::SourceMap, sync::Lrc, Mark, GLOBALS};
     use swc_ecma_parser::{EsSyntax, Parser, StringInput, Syntax, TsSyntax};
     use swc_ecma_transforms::{
         compat::{es2016, es2017, es2018, es2019, es2020, es2021, es2022},
+        helpers::{Helpers, HELPERS},
         resolver,
     };
     use swc_ecma_transforms_react::{react, Options, Runtime};
@@ -59,48 +60,53 @@ pub mod swc {
         };
 
         GLOBALS.set(&Default::default(), || {
-            try_with_handler(cm.clone(), Default::default(), |_handler| {
-                let input = StringInput::new(source_text, Default::default(), Default::default());
-                let mut program = Parser::new(syntax, input, Some(&comments))
-                    .parse_program()
-                    .unwrap();
+            try_with_handler(cm.clone(), Default::default(), |handler| {
+                HELPERS.set(&Helpers::new(true), || {
+                    HANDLER.set(handler, || {
+                        let input =
+                            StringInput::new(source_text, Default::default(), Default::default());
+                        let mut program = Parser::new(syntax, input, Some(&comments))
+                            .parse_program()
+                            .unwrap();
 
-                let top_level_mark = Mark::new();
-                let unresolved_mark = Mark::new();
+                        let top_level_mark = Mark::new();
+                        let unresolved_mark = Mark::new();
 
-                program.visit_mut_with(&mut resolver(
-                    unresolved_mark,
-                    top_level_mark,
-                    syntax.typescript(),
-                ));
+                        program.visit_mut_with(&mut resolver(
+                            unresolved_mark,
+                            top_level_mark,
+                            syntax.typescript(),
+                        ));
 
-                let mut ast_pass = (
-                    strip(unresolved_mark, top_level_mark),
-                    react(
-                        Arc::clone(&cm),
-                        Some(comments),
-                        Options {
-                            runtime: Some(Runtime::Automatic),
-                            ..Options::default()
-                        },
-                        top_level_mark,
-                        unresolved_mark,
-                    ),
-                    es2022(Default::default(), unresolved_mark),
-                    es2021(),
-                    es2020(Default::default(), unresolved_mark),
-                    es2019(),
-                    es2018(Default::default()),
-                    es2017(Default::default(), unresolved_mark),
-                    es2016(),
-                );
-                let program = program.apply(&mut ast_pass);
+                        let mut ast_pass = (
+                            strip(unresolved_mark, top_level_mark),
+                            react(
+                                Arc::clone(&cm),
+                                Some(comments),
+                                Options {
+                                    runtime: Some(Runtime::Automatic),
+                                    ..Options::default()
+                                },
+                                top_level_mark,
+                                unresolved_mark,
+                            ),
+                            es2022(Default::default(), unresolved_mark),
+                            es2021(),
+                            es2020(Default::default(), unresolved_mark),
+                            es2019(),
+                            es2018(Default::default()),
+                            es2017(Default::default(), unresolved_mark),
+                            es2016(),
+                        );
+                        let program = program.apply(&mut ast_pass);
 
-                let printed = compiler
-                    .print(&program, PrintArgs::default())
-                    .expect("print failed");
+                        let printed = compiler
+                            .print(&program, PrintArgs::default())
+                            .expect("print failed");
 
-                Ok(printed.code)
+                        Ok(printed.code)
+                    })
+                })
             })
             .unwrap()
         })
